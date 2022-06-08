8aaa671846503c0ed5ecca13ea01e13553e31985
NervousBurger <53844875+NervousBurger@users.noreply.github.com>
2022-06-09 04:27:33 +1000

    Update 选择排序.py

--- 选择排序.py
+++ 选择排序.py
@@ -15,4 +15,16 @@ def selectionSort(arr):
         arr.pop(findSmallest(arr))
     return newArr
     
+
+def selectionSort_1(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    
+    return arr
+
 print(selectionSort([5,3,6,2,10]))
+print(selectionSort_1([5,3,6,2,10]))
